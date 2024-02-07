0d5c5c2a9d490e2eae97116ee9a1497b48297967
dgimenez20 <158026468+dgimenez20@users.noreply.github.com>
2024-02-07 10:16:59 +0100

    Update buclesex2.sh

--- buclesex2.sh
+++ buclesex2.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-
+#demano cadena de nombres
 read -p "INTRODUEIX NOMBRES ENTERS: " cadena
-
+#creo un contador
 count=0
-
+#faig un bucle recorrent un per un els numeros introduits en la cadena
 for num in $cadena; do
+#si el numero es igual a zero sumo un al contador
  if  [ $num -eq 0 ]; then
   count2=$(($count + 1))
   count=count2
  fi
 done
-
+#printo el contador
 echo $count2
